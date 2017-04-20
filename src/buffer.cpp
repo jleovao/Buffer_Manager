76dc1bd54736b24c1d1b0febb0306541a024ff5a
Joseph Leovao <jleovao@ucsd.edu>
2017-04-20 16:57:38 -0700

    Started readPage implementation

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -49,53 +49,75 @@ void BufMgr::allocBuf(FrameId & frame)
     bool pageFound = false;
     std::uint32_t numPinned = 0;
    
-    try {
-    while(!pageFound) {
-        // Advance the clock to next frame
-        advanceClock();
-
-        // check if valid is set
-        if(bufDescTable[clockHand].valid) {
-           // if valid check refbit and see if was recently referenced.
-           if(bufDescTable[clockHand].refbit) {
-               // Reset refbit
-               bufDescTable[clockHand].refbit = 0;
-               continue;
-           }
-           else {
-               // check if page is pinned
-               if(bufDescTable[clockHand].pinCnt > 0) {
-                   numPinned++;
-                   continue;
-               }
-               else {
-                   // Check if dirty bit is set
-                   if(bufDescTable[clockHand].dirty) {
-                       //FlushFile(File * fileptr);
-                       //frame->Set(File * fileptr, PageID pageNum);
-                       pageFound = true;
-                   }
-                   else {
-                       //frame->Set(File * fileptr, PageID pageNum);
-                       pageFound = true;
-                   } 
-               }
-           }
-        }
-        // Valid not set
-        else {
-            //frame->Set(File * filePtr, PageID pageNum);   
-            pageFound = true;
-        }
-    }
-    catch(buffer_exceeded_exception e) {
-       
-    }
+    //try {
+        while(!pageFound) {
+	    // Advance the clock to next frame
+	    advanceClock();
+	    
+       	    // check if valid is set
+       	    if(bufDescTable[clockHand].valid) {
+       	        // if valid check refbit and see if was recently referenced.
+       	        if(bufDescTable[clockHand].refbit) {
+       	            // Reset refbit
+       	            bufDescTable[clockHand].refbit = 0;
+       	            continue;
+       	        }
+       	        else {
+                    // check if page is pinned
+            	    if(bufDescTable[clockHand].pinCnt > 0) {
+            	        numPinned++;
+             	        continue;
+             	    }
+             	    else {
+             	        // Check if dirty bit is set
+             	        if(bufDescTable[clockHand].dirty) {
+             	            //FlushFile(File * fileptr);
+             	            //bufDescTable[clockHand].Set(File * fileptr, PageID pageNum);
+             	            pageFound = true;
+             	        }
+             	        else {
+       	              	    //bufDescTable[clockHand].Set(File * fileptr, PageID pageNum);
+    	                    pageFound = true;
+    	                } 
+    	            }
+    	        }
+    	    }
+    	    // Valid not set
+    	    else {
+    	        //bufDescTable[clockHand].Set(File * filePtr, PageID pageNum);   
+    	        pageFound = true;
+    	    }
+    	} // end while 
+    //if(numPinned >= numBufs) {
+    //    throw buffer_exceeded_exception("All buffer frames pinned!");
+    //}
+    //} catch(buffer_exceeded_exception e) {
+    
+    //}
 }
 
 	
 void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 {
+        
+    //try{
+        // First check whether the page is already in the buffer pool    
+        FrameId frameNo;
+        hashTable->lookup(file, pageNo, frameNo);
+        // Set the appropriate refbit
+        bufDescTable[frameNo].refbit = true;
+        //Increment pin count for the page
+        bufDescTable[frameNo].pinCount++;
+        // return pointer to the frame containing the page via page param
+        
+    //} catch(hash_not_found_exception e) {
+        // If page is not in the buffer pool
+        // Call allocBuf(frameNo);
+        // call file->readPage(pageNo)
+        // insert page into hashtable: insert->();
+        // invoke Set() on the frame to set it up properly
+        // Return a pointer to the frame containing the page via page param
+    //}
 }
 
 
